2fc942767ae93d05964a3234169dbd0a42e92b18
Eureka Code <batagapaul10@gmail.com>
2023-10-08 17:56:51 +0800

    updated getting string from rank image

--- Main.py
+++ Main.py
@@ -63,7 +63,6 @@ def test_all_images():
             img_path = os.path.join(root, file)
             image = cv2.imread(img_path)
             image = cv2.resize(image, (1920, 1080))
-
             thresh = ip.process_card_image(image)
             conts = ip.get_card_contours(thresh)
 
